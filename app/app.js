e8ddd8bb0020cab4c5213788b19156d81d6589d2
hiwaypark <hiwaypark@gmail.com>
2022-10-07 11:12:01 +0900

    지니어스 초기화

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//라우터 설정
+const router = require("./src/routes");
+app.use("/", router);
+
+module.exports = app;
